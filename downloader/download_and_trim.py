e30fabea3a3f8f561a4a2e4b05a5f07ff697c8d8
jjokah <johnjohnsonokah@gmail.com>
2023-08-05 16:55:29 +0100

    Add script that downloads, trims and resize videos

---
+++ downloader/download_and_trim.py
@@ -0,0 +1,49 @@
+"""
+Downloads a YouTube video and extracts a clip from it.
+
+Parameters:
+url (str): The URL of the YouTube video to download.
+options (dict): A dictionary of options to pass to yt_dlp for downloading the video.
+start_time (str): The start time of the desired clip in HH:MM:SS format.
+end_time (str): The end time of the desired clip in HH:MM:SS format.
+
+Functionality:
+Uses yt_dlp to download the video from the given URL.
+Loads the downloaded video using VideoFileClip.
+Extracts the clip from start_time to end_time.
+Resizes the height of the clip to 360 pixels.
+Sets the FPS to 24.
+Writes the clipped video to compressed_clip.mp4.
+
+Requirements:
+pip install yt-dlp
+pip install moviepy
+"""
+import yt_dlp
+from moviepy.editor import VideoFileClip
+
+# Download the Video
+url = "https://www.youtube.com/watch?v=QF59xcceqrc"
+options = {
+    "format": "best[height<=720]",
+    "outtmpl": "video.mp4",
+}
+with yt_dlp.YoutubeDL(options) as ydl:
+    ydl.download([url])
+
+
+# Load the video
+video = VideoFileClip("video.mp4")
+
+# Define the start and end times of the desired clip in seconds
+start_time = "00:03:12"
+end_time = "00:03:25"
+
+# Extract the desired clip
+clip = video.subclip(start_time, end_time)
+
+# Set the output video parameters
+clip = clip.resize(height=360)  # resize the height to 360 pixels
+
+# Write the output video
+clip.write_videofile("clip.mp4")
